7a2f779133de902c76b096d1660c9dff48d807df
Kaushik Kulkarni <kaushikcfd@gmail.com>
2020-08-18 03:22:05 -0500

    add tests for offloading

--- tests/test_offloading.py
+++ tests/test_offloading.py
@@ -1,8 +1,13 @@
+import pytest
 from firedrake import *
+from pyop2.gpu.cuda import cuda_backend
+from pyop2.sequential import cpu_backend
+# from pyop2.gpu.opencl import opencl_backend
 
 
-def test_nonlinear_variational_solver():
-    from pyop2.gpu.cuda import cuda_backend as cuda
+@pytest.mark.parametrize("offloading_backend", [cuda_backend, cpu_backend])
+def test_nonlinear_variational_solver(offloading_backend):
+    set_offloading_backend(offloading_backend)
     mesh = UnitSquareMesh(32, 32)
     V = FunctionSpace(mesh, "CG", 1)
     u = TrialFunction(V)
@@ -13,26 +18,50 @@ def test_nonlinear_variational_solver():
     f = Function(V)
     f.interpolate((1+8*pi*pi)*cos(x*pi*2)*cos(y*pi*2))
     L = f * v * dx
-    u_gpu = Function(V)
-    u_cpu = Function(V)
+    fem_soln = Function(V)
     sp = {"mat_type": "matfree",
           "ksp_monitor_true_residual": None,
           "ksp_converged_reason": None}
-    with offloading(cuda):
-        solve(a == L, u_gpu, solver_parameters=sp)
+    with offloading():
+        solve(a == L, fem_soln, solver_parameters=sp)
 
-    solve(a == L, u_cpu, solver_parameters=sp)
+    f.interpolate(cos(x*pi*2)*cos(y*pi*2))
 
-    assert (sqrt(assemble(dot(u_cpu - u_gpu, u_cpu - u_gpu) *
-        dx))/sqrt(assemble(dot(u_cpu, u_cpu) * dx))) < 1e-6
+    assert norm(fem_soln-f) < 1e-2
 
-    with offloading(cuda):
-        assert (sqrt(assemble(dot(u_cpu - u_gpu, u_cpu - u_gpu) *
-            dx))/sqrt(assemble(dot(u_cpu, u_cpu) * dx))) < 1e-6
+    with offloading():
+        assert norm(fem_soln-f) < 1e-2
 
 
-def test_linear_variational_solver():
-    from pyop2.gpu.cuda import cuda_backend as cuda
+@pytest.mark.parametrize("offloading_backend", [cuda_backend, cpu_backend])
+def test_linear_variational_solver(offloading_backend):
+    set_offloading_backend(offloading_backend)
+    mesh = UnitSquareMesh(32, 32)
+    V = FunctionSpace(mesh, "CG", 1)
+    u = TrialFunction(V)
+    v = TestFunction(V)
+    f = Function(V)
+    x, y = SpatialCoordinate(mesh)
+    f.interpolate((1+8*pi*pi)*cos(x*pi*2)*cos(y*pi*2))
+
+    L = assemble(f * v * dx)
+    fem_soln = Function(V)
+
+    with offloading():
+        a = assemble((dot(grad(v), grad(u)) + v * u) * dx, mat_type='matfree')
+        solve(a, fem_soln, L)
+
+    f.interpolate(cos(x*pi*2)*cos(y*pi*2))
+
+    assert norm(fem_soln-f) < 1e-2
+
+    with offloading():
+        assert norm(fem_soln-f) < 1e-2
+
+
+@pytest.mark.parametrize("offloading_backend", [cuda_backend, cpu_backend])
+def test_data_manipulation_on_host(offloading_backend):
+    set_offloading_backend(offloading_backend)
 
     mesh = UnitSquareMesh(32, 32)
     V = FunctionSpace(mesh, "CG", 1)
@@ -45,9 +74,18 @@ def test_linear_variational_solver():
     L = assemble(f * v * dx)
     fem_soln = Function(V)
 
-    with offloading(cuda):
+    with offloading():
         a = assemble((dot(grad(v), grad(u)) + v * u) * dx, mat_type='matfree')
         solve(a, fem_soln, L)
 
-    f.interpolate(cos(x*pi*2)*cos(y*pi*2))
-    assert sqrt(assemble(dot(fem_soln - f, fem_soln - f) * dx)) < 1e-2
+    fem_soln_host = fem_soln.dat.data
+    kappa = 2.0
+    fem_soln_host *= kappa  # update data on host
+
+    with offloading():
+        error = assemble(((dot(grad(v), grad(fem_soln)) + v * fem_soln)-kappa*f*v)*dx,
+                         mat_type='matfree')
+
+        assert norm(error) < 1e-6
+
+    assert norm(error) < 1e-6
